35b7aa9935a98aa016ba6c5377e4c1320cce6326
shaitanka13 <122038076+shaitanka13@users.noreply.github.com>
2023-03-27 12:39:06 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'Ввести валидный email'       
+    valid_password = 'Ввести валидный пароль'   
+    valid_email_reg = 'Ввести валидный email'   
+    invalid_email = 'reka@mail.ru'
+    invalid_password = 'techet2407'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Пётр'
+    last_name = 'Иванов'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'г'
+    latin_generate_string = 'aattoy'
+    chinese_chars = '形声字/形聲字象形字'
+    special_chars = '!@#$%^&*()_+=/{}'
+    numbers = 659
+    empty = ''
+    passw1 = 'QwE12345'
+    passw2 = 'Zwa987456vBn09Ds'
+    passw3 = 'WaSwsX1987654321Ujb30'
